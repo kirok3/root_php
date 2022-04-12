7605a1a3e112ae8611e6e1fb6915f5591eb6d8ea
Everton Ferreira <evertonfk3@gmail.com>
2022-04-11 21:06:31 -0300

    Exercicio Ques. 3
    Questão 3 do Exercicio em PHP.

--- aulaphp/exercicio/exerciciotres.php
+++ aulaphp/exercicio/exerciciotres.php
@@ -5,24 +5,34 @@
         public $qtdAtual;
         public $qtdMinima;
 
-        public function mudarnome(nome){
+        public function mudarnome($nome){
+            $this -> nome = $nome;
+
 
         }
-        public function mudarQtdMinima(qtdMinima){
-            $this->qtdAtual
+        public function mudarQtdMinima($qtdMinima){
+            $this->qtdAtual = $qtdMinima;
+
         }
         public function repor($qtd){
-            $this->qtdAtual = $this->qtdAtual + $qtd;
             $this->qtdAtual += $qtd;
+        
         }
-        public function darBaixa(qtd){
-
+        public function darBaixa($qtd){
+            $this -> qtdAtual -= $qtd;
+        
         }
         public function mostrar(){
-
+            return "nome: " . $this->nome . "<br>".
+            "Qtd atual: " . $this->qtdAtual . "<br>".
+            "Qtd Minima: " . $this->qtdMinima;
         }
         public function precisaRepor(){
-
+            if ($this->qtdAtual <= $this->qtdMinima){
+                return true;
+            } else {
+                return false;
+            }
         }
 
     }
